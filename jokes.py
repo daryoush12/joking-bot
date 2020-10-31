34f082a145dca8cd3fd47e49e8dbc1c00165e099
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-10-31 13:02:05 +0200

    modify joke return pattern

--- jokes.py
+++ jokes.py
@@ -4,9 +4,9 @@ import requests
 def getJoke():
     resp = requests.get('https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist')
 
-    if resp.status_code != 200:
-        return None;
-    else:
+    try:
         fmt = "Q: {} \nA: {}"
         rs = resp.json()
         return fmt.format(rs['setup'], rs['delivery'])
+    except:
+        return "I was not able to access my joke bank."
